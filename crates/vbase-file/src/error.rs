b2dffead0d1cc78c0a033218d0ffa7d29ae6999a
Huachao Huang <huachao.huang@gmail.com>
2025-11-14 00:08:18 +0800

    vbase-file: add error

---
+++ crates/vbase-file/src/error.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+use thiserror::Error;
+
+/// Errors for file operations.
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("{context}: {source}")]
+    Io { source: io::Error, context: String },
+    #[error("{name} is corrupted: {message}")]
+    Corrupted { name: String, message: String },
+    #[error("{0}")]
+    InvalidArgument(String),
+}
+
+impl Error {
+    pub fn io<C>(source: io::Error, context: C) -> Self
+    where
+        C: Into<String>,
+    {
+        Self::Io {
+            source,
+            context: context.into(),
+        }
+    }
+
+    pub fn corrupted<N, M>(name: N, message: M) -> Self
+    where
+        N: Into<String>,
+        M: Into<String>,
+    {
+        Self::Corrupted {
+            name: name.into(),
+            message: message.into(),
+        }
+    }
+}
+
+/// A specialized [`std::result::Result`] for file operations.
+pub type Result<T, E = Error> = std::result::Result<T, E>;
