7be3106f0fed0c3fbe4fdf93398fa47d57354009
Huachao Huang <huachao.huang@gmail.com>
2025-11-22 01:31:29 +0800

    Add vbase-util

---
+++ crates/vbase-util/src/lib.rs
@@ -0,0 +1,13 @@
+pub use concurrent::*;
+
+#[cfg(feature = "shuttle")]
+mod concurrent {
+    pub use shuttle::sync;
+    pub use shuttle::thread;
+}
+
+#[cfg(not(feature = "shuttle"))]
+mod concurrent {
+    pub use std::sync;
+    pub use std::thread;
+}
